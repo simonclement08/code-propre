d1afec05dd20d6e46cb622da9b18a4e2001e858b
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-26 12:20:02 +0200

    TP1 - Ex4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -5,15 +5,9 @@ import java.util.Date;
 
 public class DateUtils {
 
-	public static String format(String pattern, Date date) {
-		
+	public static String format(Date date, String pattern) {
+		pattern = pattern == null ? "dd/MM/yyyy HH:mm:ss" : pattern;
 		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
-		return formateur.format(date);
-	}
-	
-	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		return formateur.format(date);
 	}
 }
