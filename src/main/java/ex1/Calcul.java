de747c6b88466dcd0938053e4c0c41d66fc69da0
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-26 11:17:55 +0200

    TP1 - Ex1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,24 @@
+package ex1;
+
+public class Calcul {
+
+	/**
+	 * Permet de calculer la somme de deux entiers
+	 * @param a
+	 * @param b
+	 * @return la somme
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+	
+	/**
+	 * Permet de calculer la différence entre deux entiers
+	 * @param a
+	 * @param b
+	 * @return la différence
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,26 @@
+package ex1;
+
+import java.util.Date;
+
+public class Entreprise {
+
+	/** Siret de l'entreprise */
+	public int siret;
+	/** Nom de l'entreprise */
+	public String nom;
+	/** Adresse de l'entreprise */
+	public String adresse;
+	/** Date de création de l'entreprise */
+	public Date dateCreation;
+
+	/** Capital max de l'entreprise */
+	public static final int CAPITAL_MAX = 3000000;
+	
+	/**
+	 * Affiche le statut
+	 */
+	public void afficherStatut(){
+		
+	}
+	
+}
