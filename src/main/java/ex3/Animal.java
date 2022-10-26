29f29931749146942d47cebfe5acaf29058ddd6a
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-26 12:09:47 +0200

    TP1 - Ex3

---
+++ src/main/java/ex3/Animal.java
@@ -0,0 +1,70 @@
+package ex3;
+
+public class Animal {
+	/** Nom de l'animal */
+	private String nom;
+	/** Type de l'animal */
+	private String type;
+	/** Comportement de l'animal */
+	private String comportement;
+	
+	/**
+	 * @param nom : nom de l'animal
+	 * @param type : type de l'animal
+	 * @param comportement : comportement de l'animal
+	 */
+	public Animal(String nom, String type, String comportement) {
+		super();
+		this.nom = nom;
+		this.type = type;
+		this.comportement = comportement;
+	}
+
+	@Override
+	public String toString() {
+		return this.getNom() + " " + this.getType() + " " + this.getComportement();
+	}
+
+	/** Getter pour l'attribut nom
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/** Setter pour l'attribut nom
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/** Getter pour l'attribut type
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/** Setter pour l'attribut type
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/** Getter pour l'attribut comportement
+	 * @return the comportement
+	 */
+	public String getComportement() {
+		return comportement;
+	}
+
+	/** Setter pour l'attribut comportement
+	 * @param comportement the comportement to set
+	 */
+	public void setComportement(String comportement) {
+		this.comportement = comportement;
+	}
+	
+}
--- src/main/java/ex3/Zoo.java
+++ src/main/java/ex3/Zoo.java
@@ -4,42 +4,79 @@ import java.util.List;
 
 public class Zoo {
 
+	/** Nom du zoo */
 	private String nom;
-	private List<String> types;
-	private List<String> noms;
-	private List<String> comportements;
-	
-	public Zoo(String nom){
+	/** Liste des animaux */
+	private List<Animal> animals;
+
+	/**
+	 * @param nom     : nom du zoo
+	 */
+	public Zoo(String nom) {
+		super();
 		this.nom = nom;
 	}
 	
-	public void addAnimal(String nomAnimal, String typeAnimal, String comportement){
-		types.add(typeAnimal);
-		noms.add(nomAnimal);
-		comportements.add(comportement);
-	}
-	
-	public void afficherListeAnimaux(){
-		for (int i=0; i<types.size(); i++) {
-			System.out.println(noms.get(i)+" "+types.get(i)+" "+comportements.get(i));
-		}
-	}
-	
-	public int taille() {
-		return types.size();
+	/**
+	 * @param nom     : nom du zoo
+	 * @param animals : liste des animaux dans le zoo
+	 */
+	public Zoo(String nom, List<Animal> animals) {
+		super();
+		this.nom = nom;
+		this.animals = animals;
 	}
 
-	/** Getter for nom
+	public void addAnimal(Animal animal) {
+		animals.add(animal);
+	}
+
+	/**
+	 * Affiche les animaux présents dans le zoo
+	 */
+	public void afficherListeAnimaux() {
+		for (Animal animal : animals) {
+			System.out.println(animal);
+		}
+	}
+
+	/**
+	 * @return le nombre de type d'animaux dans le zoo
+	 */
+	public int taille() {
+		return animals.size();
+	}
+
+	/**
+	 * Getter for nom
+	 * 
 	 * @return the nom
 	 */
 	public String getNom() {
 		return nom;
 	}
 
-	/** Setter
+	/**
+	 * Setter
+	 * 
 	 * @param nom the nom to set
 	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
+	/** Getter pour l'attribut animals
+	 * @return the animals
+	 */
+	public List<Animal> getAnimals() {
+		return animals;
+	}
+
+	/** Setter pour l'attribut animals
+	 * @param animals the animals to set
+	 */
+	public void setAnimals(List<Animal> animals) {
+		this.animals = animals;
+	}
+	
 }
